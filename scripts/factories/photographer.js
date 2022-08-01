a914568409633541a043979e227b246e04c5654b
anais meirone <anais.meirone@live.fr>
2022-08-01 10:26:51 +0200

    testing some function to retrieve data with  photographer id url

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,26 +1,42 @@
 function photographerFactory(data) {
-    const { name, portrait, city, country, tagline, price } = data;
+    const { name, id, portrait, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/photographers ID Photos/${portrait}`;
+    const photographerPage = `./photographer.html?id=${id}`;
+
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+
+        const a = document.createElement('a');
+        a.setAttribute("href", photographerPage );
+        a.classList.add('photographer-link');
+
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
+        img.setAttribute("alt", "")
+
         const h2 = document.createElement( 'h2' );
         const photographerCity = document.createElement('p');
+        photographerCity.classList.add('photographer-city');
         const photographerTagline = document.createElement('span');
         const photographerPrice = document.createElement('p');
+        photographerPrice.classList.add('photographer-price');
+
+
         h2.textContent = name;
         photographerCity.textContent = `${city}, ${country}`;
         photographerTagline.textContent = tagline;
-      photographerPrice.textContent = `${price}€/jour`;
-        article.appendChild(img);
-        article.appendChild(h2);
+        photographerPrice.textContent = `${price}€/jour`;
+
+        article.appendChild(a);
+        a.appendChild(img);
+        a.appendChild(h2);
         article.appendChild(photographerCity);
         article.appendChild(photographerTagline);
         article.appendChild(photographerPrice);
+
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, id, photographerPage, picture, getUserCardDOM }
 }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,34 @@
-//Mettre le code JavaScript lié à la page photographer.html
+// retrieve url params
+const url_params_id = window.location.search;
+console.log(url_params_id);
+
+// use urlsearchparams.get() method to extract id from url params
+ const paramsId= new URLSearchParams(url_params_id);
+ console.log(paramsId);
+
+ const photographerId = paramsId.get("id");
+  console.log(photographerId);
+
+// get photographer data
+let photographerData = [];
+async function fetchPhotographer() {
+  let response = await fetch('data/photographers.json')
+  .then((response) =>
+  response.json())
+  .then((promise) => {
+    photographerData = promise['photographers'];
+    })
+    console.log(photographerData);
+  };
+
+// display photographer data
+const photographerDisplay = async () => {
+  await fetchPhotographer();
+  const photographerHeader = document.querySelector('.photographer-header')
+  photographerHeader.innerHTML = `<article><p>${photographerData.name} </p></article>`
+  console.log(photographerHeader);
+
+
+}
+
+photographerDisplay();
